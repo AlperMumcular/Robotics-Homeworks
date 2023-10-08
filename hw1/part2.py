c5887e312d3b0e39a8425cb4890e9551ae689ee8
Alper Mumcular <72230736+AlperMumcular@users.noreply.github.com>
2023-10-09 00:18:55 +0300

    small modifications

--- hw1/part2.py
+++ hw1/part2.py
@@ -5,25 +5,5 @@ import numpy as np
 
 C = ry.Config()
 C.addFile('HW1-two-link.g')
-
-target = C.frame("target")
-joint_angles = 2 * np.pi * np.random.rand(3)
-
-def forward_kinematics(q):
-    q0 = q[0]
-    q1 = q[1]
-    y = np.sin(q0) + np.sin(q0+q1)
-    z = np.cos(q0) + np.cos(q0+q1)
-    return np.array([0, y, z])
-
-pos_target = forward_kinematics(joint_angles)
-target.setPosition(pos_target)
-C.setJointState(joint_angles)
 C.view()
-
-q = np.zeros(C.getJointDimension())
-C.setJointState(q)
-target.setPosition([0,0,0])
-
-time.sleep(5)
-
+C.watchFile('HW1-two-link.g')
--- hw1/part2c.py
+++ hw1/part2c.py
@@ -6,7 +6,10 @@ import numpy as np
 def calculate_Jacobian(q):
 	q0 = q[0]
 	q1 = q[1]
-	jacobian_matrix = np.array([[np.cos(q0) + np.cos(q0+q1),np.cos(q0+q1)],[-np.sin(q0) - np.sin(q0+q1),-np.sin(q0+q1)]])
+    # derivative of (with respect to q0) np.sin(q0) + np.sin(q0+q1) is np.cos(q0) + np.cos(q0+q1) , (with respect to q1) np.cos(q0+q1)
+    # derivative of (with respect to q0) np.cos(q0) + np.cos(q0+q1) is -np.sin(q0) - np.sin(q0+q1) , (with respect to q1) -np.sin(q0+q1)
+	jacobian_matrix = np.array([ [np.cos(q0) + np.cos(q0+q1) , np.cos(q0+q1)] ,
+                                 [-np.sin(q0) - np.sin(q0+q1) , -np.sin(q0+q1)] ])
 	return jacobian_matrix
 
 joint_angles= [1.95205226, 3.15753276,0]
